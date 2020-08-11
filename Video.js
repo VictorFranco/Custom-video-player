09cf156362f65ce67e644ff6002cb920739423fc
Victor Franco <vfrancohdez1@hotmail.com>
2020-08-11 16:06:17 -0500

    The object was converted into a js prototype

--- Video.js
+++ Video.js
@@ -1,33 +1,25 @@
 (()=>{
-    const video={
-        line_w:0,
-        id_time:null,
-        video_elm:null,
-        video_tag:null,
-        play_pause_btn:null,
-        line_time:null,
-        line_sound:null,
-        current_time_elm:null,
-        counter_time:null,
-        duration_elm:null,
-        counter_visible:false,
-        counter_sound:null,
-        volume_btn:null,
-        full_screen_btn:null,
-        is_full_screen:false,
-        execute:function(){
-            this.video_elm=document.querySelector("#video");
-            this.video_tag=document.querySelector("video");
-            this.play_pause_btn=document.querySelector("#play_pause");
-            this.line_time=document.querySelector("#line");
-            this.line_sound=document.querySelector("#volume");
-            this.counter_time=document.querySelector("#counter");
-            this.counter_sound=document.querySelector("#counter_s");
-            this.current_time_elm=document.querySelector("#current_time");
-            this.duration_elm=document.querySelector("#duration");
-            this.full_screen_btn=document.querySelector("#full_screen");
-            this.volume_btn=document.querySelector("#btn_volume");
-
+    document.querySelectorAll(".video").forEach(elm=>{
+        let video=new Video(elm);
+        video.execute();
+    });
+    function Video(videoElm){
+        this.line_w=0;
+        this.id_time=null;
+        this.counter_visible=false;
+        this.is_full_screen=false;
+        this.video_elm=videoElm;
+        this.video_tag=this.video_elm.children[0];
+        this.play_pause_btn=this.video_elm.children[1].children[0];
+        this.line_time=this.video_elm.children[1].children[1];
+        this.line_sound=this.video_elm.children[1].children[3].children[1].children[0];
+        this.current_time_elm=this.video_elm.children[1].children[2].children[0];
+        this.counter_time=this.video_elm.children[1].children[1].children[1];
+        this.duration_elm=this.video_elm.children[1].children[2].children[1];
+        this.counter_sound=this.video_elm.children[1].children[3].children[1].children[0].children[1];
+        this.volume_btn=this.video_elm.children[1].children[3].children[0];
+        this.full_screen_btn=this.video_elm.children[1].children[4];
+        this.execute=function(){
             this.full_screen_btn.addEventListener("click",this.full_screen.bind(this));
             this.volume_btn.addEventListener("click",this.mute_unmute.bind(this));
             this.volume_btn.addEventListener("keyup",e=>{
@@ -55,29 +47,29 @@
             window.addEventListener("resize",()=>this.line_w=this.line_time.clientWidth);
             this.line_sound.addEventListener("mousedown",e=>this.limited_movement(this.volume.bind(this),e,this.line_sound));
             this.line_sound.addEventListener("keydown",e=>this.volume_keyboard_navigation(e));
-        },
-        play_pause:function(){
+        };
+        this.play_pause=function(){
             if(this.video_tag.paused) this.play();
             else this.pause();
-        },
-        play:function(){
+        };
+        this.play=function(){
             this.video_tag.play();
             this.play_pause_btn.innerHTML="&#10074;&#10074;";
             this.update();
-        },
-        pause:function(){
+        };
+        this.pause=function(){
             this.video_tag.pause();
             this.play_pause_btn.innerHTML="▶";
             clearTimeout(this.id_time);
-        },
-        update:function(){
+        };
+        this.update=function(){
             this.line_time.style.setProperty('--pos', this.video_tag.currentTime/this.video_tag.duration*100 + "%");
             this.current_time_elm.innerHTML=timer.trasform_seconds(this.video_tag.currentTime);
             this.id_time=setTimeout(this.update.bind(this),1000);
             if(this.video_tag.currentTime==this.video_tag.duration) this.pause();
             if(this.counter_visible) this.counter_time.innerHTML=timer.trasform_seconds(this.video_tag.currentTime);
-        },
-        move:function(e){
+        };
+        this.move=function(e){
             let pos=(e.pageX-this.line_time.getBoundingClientRect().left)/this.line_w*100;
             if(pos>100) pos=100;
             else if(pos<0) pos=0;
@@ -85,8 +77,8 @@
             this.keeps_pause_of(()=>this.video_tag.currentTime=pos/100*this.video_tag.duration);
             [this.current_time_elm,this.counter_time]
             .forEach(elm=>elm.innerHTML=timer.trasform_seconds(this.video_tag.currentTime));
-        },
-        time_keyboard_navigation:function(e){
+        };
+        this.time_keyboard_navigation=function(e){
             if(e.keyCode==39||e.keyCode==32||e.keyCode==37){
                 let current_time=this.video_tag.currentTime;
                 let duration=this.video_tag.duration;
@@ -100,8 +92,8 @@
                 [this.current_time_elm,this.counter_time]
                 .forEach(elm=>elm.innerHTML=timer.trasform_seconds(this.video_tag.currentTime));
             }
-        },
-        volume_keyboard_navigation:function(e){
+        };
+        this.volume_keyboard_navigation=function(e){
             if(e.keyCode>36&&e.keyCode<41){
                 let current_volume=this.video_tag.volume;
                 if(e.keyCode==39||e.keyCode==38)
@@ -113,21 +105,21 @@
                 this.line_sound.style.setProperty('--pos', this.video_tag.volume*100 + "%");
                 this.counter_sound.innerHTML=Math.round(this.video_tag.volume*100);   
             }
-        },
-        keeps_pause_of:function(f){
+        };
+        this.keeps_pause_of=function(f){
             if(this.video_tag.paused){
                 f();
                 this.pause();
             }else f();
-        },
-        limited_movement:function(f,evt,elm){
+        };
+        this.limited_movement=function(f,evt,elm){
             f(evt);
             if(elm.onmousemove==null)
                 elm.onmousemove=evt=>f(evt);
             if(document.onmouseup==null) 
                 document.onmouseup=()=>{elm.onmousemove=null;document.onmouseup=null};
-        },
-        volume:function(e,position){
+        };
+        this.volume=function(e,position){
             let pos=position||(this.line_sound.getBoundingClientRect().bottom-e.pageY)/this.line_sound.clientWidth*100;
             if(pos>100) pos=100;
             else if(pos<0) pos=0;
@@ -138,11 +130,11 @@
             if(pos!=0&&this.volume_btn.innerHTML!="&#128266") this.volume_btn.innerHTML="&#128266";
             else if(pos==0&&this.volume.textContent!="&#128263") this.volume_btn.innerHTML="&#128263";
         },
-        mute_unmute:function(){
+        this.mute_unmute=function(){
             if(this.video_tag.volume!=0)this.volume(undefined,"0");
             else this.volume.bind(this)(undefined,"50");
-        },
-        full_screen:function(){
+        }
+        this.full_screen=function(){
             if(this.video_elm.requestFullscreen&&document.exitFullscreen){
                 if(this.is_full_screen) document.exitFullscreen();
                 else this.video_elm.requestFullscreen();
@@ -173,5 +165,4 @@
             return num;
         }
     };
-    video.execute();     
 })();
